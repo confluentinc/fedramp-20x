eb13672d1036adf0c74a0ec9839dbac072a17c02
Robert Wittman <rwittman@confluent.io>
2025-06-13 22:12:16 -0400

    Add checks for KSI-MLA-04 and KSI-RPL-03

--- models/fedramp_ksis.sql
+++ models/fedramp_ksis.sql
@@ -16,6 +16,19 @@ with
         ({{ elasticache_clusters_should_be_encrypted_at_rest('KSI_SC', '1.3') }})
             {{ union() }}
         ({{ elasticache_replication_groups_should_be_encrypted_at_rest('KSI_SC', '1.4') }})
+            {{ union() }}
+
+        -- KSI-MLA-04: Perform authenticated vulnerability scanning on information resources
+        ({{ ec2_instances_should_be_scanned_by_inspector('KSI-MLA-04', '1.0')}})
+{{ union() }}
+        ({{ inspector_vulnerabilities_should_be_resolved_in_sla('KSI-MLA-04', '1.1') }})
+{{ union() }}
+        -- KSI-RPL-03: Perform system backups aligned with recovery objectives
+({{ rds_instances_should_have_backups_configured('KSI-RPL-03', '1.0') }})
+{{ union() }}
+({{ rds_instances_should_have_backup_vault_configured('KSI-RPL-03', '1.1') }})
+{{ union() }}
+({{ s3_buckets_should_have_backup_vault_configured('KSI-RPL-03', '1.2') }})
     )
 select
     {{ gen_timestamp() }},
