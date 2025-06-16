e279b3dafe9adb1c1df63e01d8f38ae04a116151
Robert Wittman <rwittman@confluent.io>
2025-06-16 11:49:05 -0400

    KSI-SVC-05 (#8)
    * Add KSI-SVC-04
    
    * Remove unused project settings
    
    * Use vars file for configuration, change naming of macros
    
    * Fix enforcement webhooks check
    
    * Add check that enforcement policies are not ignoring failures
    
    * Remove unneeded comments

--- models/fedramp_ksis.sql
+++ models/fedramp_ksis.sql
@@ -1,21 +1,30 @@
 with
     aggregated as (
-
         -- KSI-SVC-02: Encrypt or otherwise secure network traffic
         {{ alb_should_have_acceptable_tls_policy('KSI-SVC-02', '1.0')}}
             {{ union() }}
         {{ alb_should_redirect_plaintext_ports('KSI-SVC-02', '1.1')}}
+            {{ union() }}
 
         -- KSI-SVC-03: Encrypt all federal and sensitive information at rest
         ({{ ebs_volumes_should_be_encrypted_at_rest('KSI_SC', '1.0') }})
             {{ union() }}
-        ({{ rds_clusters_should_be_encrypted_at_rest('KSI_SC', '1.1') }})
+        ({{ rds_instances_should_be_encrypted_at_rest('KSI_SC', '1.1') }})
             {{ union() }}
         ({{ s3_buckets_should_be_encrypted_at_rest('KSI_SC', '1.2') }})
             {{ union() }}
         ({{ elasticache_clusters_should_be_encrypted_at_rest('KSI_SC', '1.3') }})
             {{ union() }}
         ({{ elasticache_replication_groups_should_be_encrypted_at_rest('KSI_SC', '1.4') }})
+            {{ union() }}
+
+        -- KSI-SVC-05: Enforce system and information resource integrity through cryptographic means
+        ({{ k8s_clusters_should_have_enforcement_webhooks('KSI-SVC-04', '1.0') }})
+            {{ union() }}
+        ({{ k8s_has_image_enforcement_policies('KSI-SVC-04', '1.1') }})
+            {{ union() }}
+        ({{ k8s_image_enforcement_policies_should_not_ignore_failures('KSI-SVC-04', '1.2') }})
+
     )
 select
     {{ gen_timestamp() }},
