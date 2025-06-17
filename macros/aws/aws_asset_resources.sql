de45a2178b66d7782792c42613bcc66766911c90
Robert Wittman <rwittman@confluent.io>
2025-06-16 20:15:47 -0400

    Add AWS asset inventory model (#14)
    * Add aws_resources model
    
    * Remove unused datasources
    
    * Dont self-reference aws_resources, fix check for S3 bucket types

---
+++ macros/aws/aws_asset_resources.sql
@@ -0,0 +1,37 @@
+{% macro aws_asset_resources(table_name, ARN_EXIST, ACCOUNT_ID_EXIST, REQUEST_ACCOUNT_ID_EXIST, REGION_EXIST, TAGS_EXIST) %}
+  {{ return(adapter.dispatch('aws_asset_resources')(table_name, ARN_EXIST, ACCOUNT_ID_EXIST, REQUEST_ACCOUNT_ID_EXIST, REGION_EXIST, TAGS_EXIST)) }}
+{% endmacro %}
+
+{%- macro bigquery__aws_asset_resources(table_name, ARN_EXIST, ACCOUNT_ID_EXIST, REQUEST_ACCOUNT_ID_EXIST, REGION_EXIST, TAGS_EXIST) -%}
+SELECT 
+_cq_id, _cq_source_name, _cq_sync_time,
+  {% if ACCOUNT_ID_EXIST %}
+    account_id
+  {% else %}
+    SPLIT(arn, ':')[SAFE_OFFSET(4)]
+  {% endif %} AS account_id,
+  {% if REQUEST_ACCOUNT_ID_EXIST %}
+    request_account_id
+  {% else %}
+    SPLIT(arn, ':')[SAFE_OFFSET(4)]
+  {% endif %} AS request_account_id,
+    CASE WHEN SPLIT(SPLIT(arn, ':')[SAFE_OFFSET(5)], '/')[SAFE_OFFSET(1)] IS NULL AND SPLIT(arn, ':')[SAFE_OFFSET(6)] IS NULL
+    THEN NULL
+    ELSE SPLIT(SPLIT(arn, ':')[SAFE_OFFSET(5)], '/')[SAFE_OFFSET(0)] END AS TYPE,
+    arn,
+  {% if REGION_EXIST %}
+    region
+  {% else %}
+    'unavailable'
+  {% endif %} AS region,
+  {% if TAGS_EXIST %}
+    TO_JSON_STRING(tags)
+  {% else %}
+    TO_JSON_STRING(STRUCT())
+  {% endif %} AS tags,
+  SPLIT(arn, ':')[SAFE_OFFSET(1)] AS PARTITIONS,
+  SPLIT(arn, ':')[SAFE_OFFSET(2)] AS service,
+  '{{ table_name | string }}' AS _cq_table
+FROM {{ full_table_name(table_name | string) }}
+WHERE {{ partition_filter() }}
+{%- endmacro -%}  
