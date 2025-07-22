cb76f4c87a2a8290aa103734cdda03f53006566f
Jason Nguyen <31643070+jnguyen1294@users.noreply.github.com>
2025-07-22 13:47:41 -0500

    Add macros to rpl-03 (#41)

--- models/fedramp_ksis.sql
+++ models/fedramp_ksis.sql
@@ -161,8 +161,16 @@ with
         -- KSI-PIY-07: Document risk management decisions for software supply chain security.
 
         -- KSI-RPL-01: Define Recovery Time Objectives (RTO) and Recovery Point Objectives (RPO).
+        ({{ db_instance_backup_enabled('KSI-RPL-01','1.0') }})
+            {{ union() }}
+        ({{ s3_buckets_should_have_backup_vault_configured('KSI-RPL-01','1.1') }})
+            {{ union() }}
         -- KSI-RPL-02: Develop and maintain a recovery plan that aligns with the defined recovery objectives.
         -- KSI-RPL-03: Perform system backups aligned with recovery objectives.
+        ({{ db_instance_backup_enabled('KSI-RPL-03','1.0') }})
+            {{ union() }}
+        ({{ s3_buckets_should_have_backup_vault_configured('KSI-RPL-03','1.1') }})
+            {{ union() }}
         -- KSI-RPL-04: Regularly test the capability to recover from incidents and contingencies.
 
         -- KSI-SVC-01: Harden and review network and system configurations
