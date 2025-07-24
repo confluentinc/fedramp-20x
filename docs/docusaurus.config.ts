07b16f631a8ed9219039d786928f342b638179d6
Robert Wittman <rwittman@confluent.io>
2025-07-24 12:11:16 -0400

    Rwittman/documentation (#42)
    * Stub out docusaurus pages
    
    * Remove unused components
    
    * Adjust page layouts
    
    * Add deployment branch
    
    * Use data backed generation of docs/models
    
    * Fix typo

---
+++ docs/docusaurus.config.ts
@@ -0,0 +1,95 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: 'Confluent - FedRAMP 20x',
+  tagline: 'KSI Evaluations with DBT',
+  favicon: 'img/confluent-icon.png',
+
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
+
+  // Set the production url of your site here
+  url: 'https://confluentinc.github.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/fedramp-20x/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'confluentinc', // Usually your GitHub org/user name.
+  projectName: 'fedramp-20x', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+        },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'FedRAMP 20x',
+      logo: {
+        alt: 'Confluent Icon',
+        src: 'img/confluent-icon.png',
+      },
+      items: [
+        {
+          position: 'left',
+          label: 'Automation',
+          to: '/',
+        },
+        {
+          type: 'docSidebar',
+          sidebarId: 'modelSidebar',
+          position: 'left',
+          label: 'Models',
+        },
+        {
+          href: 'https://github.com/confluentinc/fedramp-20x',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [],
+      copyright: `Copyright © ${new Date().getFullYear()} Confluent`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
