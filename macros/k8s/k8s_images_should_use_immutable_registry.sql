d8d04d1d4bf93fd9ca065e59959695801326f364
Robert Wittman <rwittman@confluent.io>
2025-08-22 11:22:30 -0400

    Adjust our ECR image checks

--- macros/k8s/k8s_images_should_use_immutable_registry.sql
+++ macros/k8s/k8s_images_should_use_immutable_registry.sql
@@ -6,20 +6,31 @@
 
 {% macro bigquery__k8s_images_should_use_immutable_registry(framework, check_id) %}
 with images as (
-    select image_name from {{ full_table_name("k8s_container_images") }} group by image_name
+    select REPLACE(image_name, 'ecr-fips', 'ecr') as image_name
+    from {{ full_table_name("k8s_container_images") }}
+    group by image_name
 )
 select
     '{{ framework }}' as framework,
     '{{ check_id }}' as check_id,
     'Kubernetes images should use an immutable registry' as title,
-    arn as identifier,
+    images.image_name as identifier,
     JSON_OBJECT() as metadata,
-    case when er.repository_uri IS NOT NULL and er.image_tag_mutability = 'IMMUTABLE'
-         then 'pass'
-         else 'fail'
+    case
+      when er.repository_uri IS NOT NULL and er.image_tag_mutability = 'IMMUTABLE'
+        then 'pass'
+
+      when STARTS_WITH(images.image_name, '013241004608.dkr.ecr.us-gov-west-1.amazonaws.com')
+        then 'pass'
+
+      when STARTS_WITH(images.image_name, 'gke.gcr.io')
+        then 'pass'
+
+      else 'fail'
     end as status,
     tags
 from images
 left join {{ full_table_name("aws_ecr_repositories") }} as er
-    on images.image_name = er.repository_uri
+    on images.image_name = REPLACE(er.repository_uri, 'ecr-fips', 'ecr')
+    and {{ partition_filter("er") }}
     {% endmacro %}
