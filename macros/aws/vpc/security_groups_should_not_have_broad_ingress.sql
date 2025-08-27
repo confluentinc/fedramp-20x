286810503f33e9dfe7e514a82aa3c5ee0e9eb19b
Robert Wittman <rwittman@confluent.io>
2025-08-27 09:59:24 -0400

    Only select security groups that have open ports as well as 0.0.0.0/0

--- macros/aws/vpc/security_groups_should_not_have_broad_ingress.sql
+++ macros/aws/vpc/security_groups_should_not_have_broad_ingress.sql
@@ -19,7 +19,9 @@ select
 from {{ full_table_name("aws_ec2_security_groups") }} as sg
 left join (
   select id from {{ ref('aws_compliance__security_group_ingress_rules') }}
-  where (ip = '0.0.0.0/0' or ip = '::/0') group by id
+  where (ip = '0.0.0.0/0' or ip = '::/0')
+  and (port_range_to is null or port_range_from is null)
+  group by id
 ) ingress
 on ingress.id = sg.group_id
 left join {{ full_table_name("aws_ec2_vpcs") }} as vpc on sg.vpc_id = vpc.vpc_id
