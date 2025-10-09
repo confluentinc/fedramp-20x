47d804a8fc8ac87a3a35c6de1e9ce9f7a55c87cc
Robert Wittman <rwittman@confluent.io>
2025-10-09 11:07:09 -0400

    Copilot fixes

--- macros/okta/okta_behavior_rules_detect_compromise.sql
+++ macros/okta/okta_behavior_rules_detect_compromise.sql
@@ -8,12 +8,12 @@
 select
     '{{ framework }}' as framework,
     '{{ check_id }}' as check_id,
-    CONCAT('Okta behavior rules will lock compromised accounts due to: ', name, ' (', type, ')')as title,
+    CONCAT('Okta behavior rules will lock compromised accounts due to: ', name, ' (', type, ')') as title,
     id as identifier,
     settings as metadata,
     'pass' as status, -- This isn't a pass / fail control, we simply identify the compromise controls
     JSON_OBJECT() as tags
 from {{ full_table_name("okta_behavior_rules") }}
 where {{ partition_filter() }}
-and UPPER(status) = "ACTIVE"
+and UPPER(status) = 'ACTIVE'
     {% endmacro %}
