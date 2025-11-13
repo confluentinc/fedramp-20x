777c4abcd83a69f674e65332e978714e0ac172e3
Robert Wittman <rwittman@confluent.io>
2025-11-13 12:42:39 -0500

    Add macros for some manual controls

--- macros/ksis/third_party_information_resources.sql
+++ macros/ksis/third_party_information_resources.sql
@@ -2,6 +2,7 @@
 -- KSI-TPR: Third-Party Information Resources
 -- KSI-TPR-01: Follow the requirements and recommendations in the FedRAMP Minimum Assessment Standard regarding third-party information resources
 ({{ vendors_should_have_fedramp_authorization('KSI-TPR-01', '1.0') }})
+    {{ union() }}
 
 -- KSI-TPR-03: Identify and prioritize mitigation of potential supply chain risks.
 -- KSI-TPR-04: Monitor third party software information resources for upstream vulnerabilities, with contractual notification requirements or active monitoring services.
