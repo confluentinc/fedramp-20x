2dc2ee725346d409009b40f90322f263dfd864cc
Jason Nguyen <31643070+jnguyen1294@users.noreply.github.com>
2025-11-17 16:01:23 -0600

    [KSI-CED] Adds a model that maps active users to valid training. (#86)
    * [KSI-CED] Adds a model that maps active users to valid training.
    
    The model can be read as "For each ACTIVE user in Okta, do they have
    up-to-date training in Workday? If so, pass; if not, fail."
    
    * Add partition filter for training table

--- macros/ksis/cybersecurity_education.sql
+++ macros/ksis/cybersecurity_education.sql
@@ -1,7 +1,10 @@
 {% macro cybersecurity_education() %}
 -- KSI-CED: Cybersecurity Education
 -- KSI-CED-01: Ensure all employees receive security and privacy awareness training, incident response training, and are familiar with all relevant policies and procedures.
+({{ okta_active_users_should_have_valid_training('KSI-CED-01', '1.0') }})
+    {{ union() }}
 -- KSI-CED-02: Require role-specific training for high risk roles, including at least roles with privileged access.
+({{ okta_active_users_should_have_valid_training('KSI-CED-02', '1.0') }})
 -- KSI-CED-03: Require role-specific training for development and engineering staff covering best practices for delivering secure software.
 {% endmacro %}
 
