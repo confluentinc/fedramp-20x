3bc1f0eb20944e440c32229bddaf75851219051f
Robert Wittman <rwittman@confluent.io>
2025-11-17 10:30:42 -0500

    Fix query syntax

--- macros/aws/rds/rds_instances_should_force_ssl.sql
+++ macros/aws/rds/rds_instances_should_force_ssl.sql
@@ -6,7 +6,7 @@
 
 {% macro bigquery__rds_instances_should_force_ssl(framework, check_id) %}
 with databases as (
-    select i.arn, JSON_VALUE(pg.DBParameterGroupName) as group_name, dbpg.arn as group_arn
+    select i.arn, JSON_VALUE(pg.DBParameterGroupName) as group_name, dbpg.arn as group_arn, i.tags
     from {{ full_table_name("aws_rds_instances") }} i
     left join unnest(JSON_QUERY_ARRAY(db_parameter_groups)) pg
     left join {{ full_table_name("aws_rds_db_parameter_groups") }} dbpg
@@ -25,10 +25,9 @@ select
     case when exists(
         select 1 from {{ full_table_name("aws_rds_db_parameter_group_db_parameters") }}
         where db_parameter_group_arn = group_arn
-          and parameter_name = "rds.force_ssl"
-          and parameter_value = "1"
+          and parameter_name = 'rds.force_ssl'
+          and parameter_value = '1'
     ) then 'pass' else 'fail' end as status,
-    i.tags as tags
-from {{ full_table_name("aws_rds_instances") }}
-WHERE {{ partition_filter() }}
+    tags as tags
+from databases
 {% endmacro %}
