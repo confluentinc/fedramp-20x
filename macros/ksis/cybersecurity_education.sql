c52da345bda001fe18c6e0626dfd6411c4726d59
Robert Wittman <rwittman@confluent.io>
2025-11-20 20:21:19 -0500

    Update comments for all updated controls

--- macros/ksis/cybersecurity_education.sql
+++ macros/ksis/cybersecurity_education.sql
@@ -1,10 +1,11 @@
 {% macro cybersecurity_education() %}
 -- KSI-CED: Cybersecurity Education
--- KSI-CED-01: Ensure all employees receive security and privacy awareness training, incident response training, and are familiar with all relevant policies and procedures.
+-- KSI-CED-01: Require and monitor the effectiveness of training given to all employees on policies, procedures, and security-related topics.
 ({{ okta_active_users_should_have_valid_training('KSI-CED-01', '1.0') }})
     {{ union() }}
--- KSI-CED-02: Require role-specific training for high risk roles, including at least roles with privileged access.
+-- KSI-CED-02: Require and monitor the effectiveness of role-specific training for high risk roles, including at least roles with privileged access.
 ({{ okta_active_users_should_have_valid_training('KSI-CED-02', '1.0') }})
--- KSI-CED-03: Require role-specific training for development and engineering staff covering best practices for delivering secure software.
+-- KSI-CED-03: Require and monitor the effectiveness of role-specific training provided to development and engineering staff that covers best practices for delivering secure software.
+-- KSI-CED-04: Require and monitor the effectiveness of role-specific training to staff involved with incident response or disaster recovery.
 {% endmacro %}
 
--- macros/ksis/incident_reporting.sql
+++ macros/ksis/incident_reporting.sql
@@ -1,6 +1,6 @@
 {% macro incident_reporting() %}
 -- KSI-INR: Incident Response
--- KSI-INR-01: Respond to incidents according to FedRAMP requirements and cloud service provider policies
+-- KSI-INR-01: Always follow a documented incident response procedure.
 -- KSI-INR-02: Maintain a log of incidents and periodically review past incidents for patterns or vulnerabilities.
 ({{ jira_should_have_security_project('KSI-INR-02', '1.0') }})
     {{ union() }}
